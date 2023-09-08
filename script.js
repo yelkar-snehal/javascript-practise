ff4a80da50fe1259b25fa3185a06050e2af2addb
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-09-08 11:57:44 +0530

    refactor

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 console.log("Started script");
 
 // console.log(setTimeout(() => console.log("Normal setTimeout"), 3000))
-let counter = 0;
-const onKeyDown = (value) => {
-  console.log({ value, counter });
-  console.log(this.value + " in onKeyDown");
-  counter++;
-};
+// let counter = 0;
+// const onKeyDown = (value) => {
+//   console.log({ value, counter });
+//   console.log(this.value + " in onKeyDown");
+//   counter++;
+// };
 
-const debounce = (fn, delay) => {
-  console.log(fn);
-  let timer;
-  let counter2 = 0;
-  return (...args) => {
-    const ctx = this;
-    console.log({ counter2 });
-    counter2++;
-    clearTimeout(timer);
-    timer = setTimeout(() => fn.apply(ctx, args), delay);
-  };
-};
+// const debounce = (fn, delay) => {
+//   console.log(fn);
+//   let timer;
+//   let counter2 = 0;
+//   return (...args) => {
+//     const ctx = this;
+//     console.log({ counter2 });
+//     counter2++;
+//     clearTimeout(timer);
+//     timer = setTimeout(() => fn.apply(ctx, args), delay);
+//   };
+// };
 
-const throttle = (fn, delay) => {
-  let shouldCall = true;
-  return (...args) => {
-    const ctx = this;
-    if (shouldCall) {
-      console.log(this.value + " in throttle");
-      fn.apply(ctx, args);
-      shouldCall = false;
-      setTimeout(() => (shouldCall = true), delay);
-    }
-  };
-};
+// const throttle = (fn, delay) => {
+//   let shouldCall = true;
+//   return (...args) => {
+//     const ctx = this;
+//     if (shouldCall) {
+//       console.log(this.value + " in throttle");
+//       fn.apply(ctx, args);
+//       shouldCall = false;
+//       setTimeout(() => (shouldCall = true), delay);
+//     }
+//   };
+// };
 
-const debouncedKeyDown = debounce(onKeyDown, 10000);
-const throttledKeyDown = throttle(onKeyDown, 2000);
+// const debouncedKeyDown = debounce(onKeyDown, 10000);
+// const throttledKeyDown = throttle(onKeyDown, 2000);
 
 // event bubbling and trickling
 // document
