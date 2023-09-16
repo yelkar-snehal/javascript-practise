ca593246555346468d2006eb2e0760667b37ad3f
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-09-16 16:14:27 +0530

    update closure docs

--- closure.js
+++ closure.js
@@ -1,1 +1,20 @@
 console.log("executing closure script");
+/**
+ * fn bundled along with its lexical scope
+ * fn has access to its scope even after the outer fn has been executed
+ *
+ * this can be seen in the browser while debugging in the "closure" field
+ *
+ * can be used for currying, memoise, setTieout, encapsulation
+ * infact setTimeout can access the variables ouside it because of closures
+ *
+ * memory leak is one of the cons in closure
+ * memory garbage collection doesn't always happen
+ * v8 engine optimises this
+ *
+ * to form a closure enclose everything within a function
+ *
+ * new keyword with a function for ms a ctror; good practise is to have ctor's
+ * first letter as Capital
+ *
+ */
