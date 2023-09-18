f5eb23f3a708eff93e2beea35dbad2354ec4a619
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-09-18 14:13:01 +0530

    add this use case

--- closureAndThis.js
+++ closureAndThis.js
@@ -152,3 +152,37 @@ console.log("executing closure script");
 // };
 // obj.func();
 // obj.aFunc();
+
+// const myObject = {
+//   myMethod: () => {
+//     console.log(this);
+//   },
+// };
+
+// myObject.myMethod(); // this === window or global object
+
+// const myMethod = myObject.myMethod;
+// myMethod(); // this === window or global object
+
+const myObject = {
+  myArrowFunction: null,
+  myMethod: function () {
+    this.myArrowFunction = () => {
+      console.log(this);
+    };
+  },
+};
+
+myObject.myMethod(); // this === myObject
+
+myObject.myArrowFunction(); // this === myObject
+
+const myArrowFunction = myObject.myArrowFunction;
+myArrowFunction(); // this === myObject
+
+/**
+ * When we call myObject.myMethod(),
+ * we initialize myObject.myArrowFunction with an arrow function which is inside of the method myMethod,
+ * so it will inherit its scope.
+ * We can clearly see a perfect use case, closures.
+ */
