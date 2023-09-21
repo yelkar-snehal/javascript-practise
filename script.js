fecdb5c348b99abae44ba992644e975dcc7c28d6
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-09-21 15:43:17 +0530

    refactor iife script

--- script.js
+++ script.js
@@ -156,38 +156,6 @@ console.log("Started script");
 
 // console.log(bigBrother());
 
-//fn hoisting and iife
-
-// let i = "foo";
-
-// (function () {
-//   console.log("Original value was: " + i);
-//   let i = "bar";
-//   console.log("New value is: " + i);
-// })();
-
-/**
- * Original value was: undefined
-   New value is: bar
-
-   The IIFE is an expression, not a statement, so no it is not hoisted.
-
-   var i inside the IIFE is hoisted to the top of the function scope, 
-   but the assignment is not. The following is equivalent:
-
-   this is equivalent to:
-
-   (function(){
-    var myVar;
-    console.log('Original value was: '+ myVar);
-    myVar = 'bar';
-    console.log('New value is: ' + myVar);
-   })();
-
-   therefore if tried with let it would throw the below error
-   
- */
-
 //  pass by val
 // var c = 5;
 // var d = c;
