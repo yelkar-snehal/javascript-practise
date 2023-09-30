7e52719093b7d7546c041fd5bcb8020bc31db369
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-09-30 08:26:48 +0530

    add global ts declarations

---
+++ dsa/global.d.ts
@@ -0,0 +1,9 @@
+declare interface List<T> {
+  get length(): number;
+  removeAt(index: number): T | undefined;
+  remove(item: T): T | undefined;
+  get(index: number): T | undefined;
+  prepend(item: T): void;
+  append(item: T): void;
+  insertAt(item: T, idx: number): void;
+}
