0fca7dbfd9e491079e7d4e540fc53adac9215028
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-11-21 10:41:07 +0530

    add bt tree boilerplate

---
+++ dsa/BTPreOrder.ts
@@ -0,0 +1,1 @@
+export default function pre_order_search(head: BinaryNode<number>): number[] {}
--- dsa/global.d.ts
+++ dsa/global.d.ts
@@ -13,3 +13,9 @@ declare type Point = {
   x: number;
   y: number;
 };
+
+declare type BinaryNode<T> = {
+  value: T;
+  left: BinaryNode<T> | null;
+  right: BinaryNode<T> | null;
+};
