ea5df6b64f30a21d08ce008b8bfde99b868226cd
yelkar-snehal <yelkarsnehal9@gmail.com>
2023-11-21 11:01:20 +0530

    add binary tree pre order traversal eg

--- dsa/BTPreOrder.ts
+++ dsa/BTPreOrder.ts
@@ -1,1 +1,26 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {}
+/**
+ * @param currNode curent node
+ * @param path mutable traversed path
+ * @returns mutataed path
+ */
+function traverse(
+  currNode: BinaryNode<number> | null,
+  path: number[]
+): number[] {
+  // base case, traversed beyond the tree
+  if (currNode === null) return path;
+
+  // pre
+  path.push(currNode.value);
+
+  // recurse
+  traverse(currNode.left, path);
+  traverse(currNode.right, path);
+
+  // post
+  return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+  return traverse(head, []);
+}
