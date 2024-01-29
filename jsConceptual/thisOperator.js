395a9e9cbe28068421153d11672621bf7fa96fde
yelkar-snehal <yelkarsnehal9@gmail.com>
2024-01-29 11:49:50 +0530

    add this in fn with strict/non strict eg

--- jsConceptual/thisOperator.js
+++ jsConceptual/thisOperator.js
@@ -1,10 +1,12 @@
-//  global context
+// "use strict";
 
+//  global context
 // prints the reference to the global object; browser: window, node: runntime env
 console.log(this);
 
 // functionol context
 function foo() {
+  //  inisde the fn this reference changes w.r.t. the mode strict/non strict, undefined/global(i.e enclosing context)
   console.log(this);
 }
 foo();
