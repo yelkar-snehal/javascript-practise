cf59c3fae97523393a2a6bf94c24cb8b2accfe59
snehaly <snehaly@users.noreply.github.com>
2026-05-02 14:52:35 +0530

    add custom scripts

--- jsExercises/customThrottle.js
+++ jsExercises/customThrottle.js
@@ -1,5 +1,37 @@
-function myThrottle(cb, interval) {
+// function myThrottle(cb, interval) {
+//   let isThrottled = false;
+
+//   return function (...args) {
+//     if (!isThrottled) {
+//       cb.apply(this, args);
+//       isThrottled = true;
+
+//       setTimeout(() => {
+//         isThrottled = false;
+//       }, interval);
+//     }
+//   };
+
+// }
+
+// Example usage:
+const logMessage = (message) => {
+  console.log(message);
+};
+
+// const throttledLogMessage = myThrottle(logMessage, 3000);
+
+// throttledLogMessage("Hello, World!"); // Will log "Hello, World!" immediately
+// throttledLogMessage("Hello again!"); // Will be ignored since it's within the throttle interval
+
+// setTimeout(() => {
+//   throttledLogMessage("Hello after 3 seconds!"); // Will log "Hello after 3 seconds!" after 3 seconds
+// }, 3000);
+
+// trailing edge throttle
+function myThrottleTrailing(cb, interval) {
   let isThrottled = false;
+  let lastArgs = null;
 
   return function (...args) {
     if (!isThrottled) {
@@ -8,22 +40,60 @@ function myThrottle(cb, interval) {
 
       setTimeout(() => {
         isThrottled = false;
+        if (lastArgs) {
+          cb.apply(this, lastArgs);
+          lastArgs = null;
+        }
       }, interval);
+    } else {
+      lastArgs = args;
     }
   };
 
 }
 
 // Example usage:
-const logMessage = (message) => {
-  console.log(message);
-};
+const throttledLogMessageTrailing = myThrottleTrailing(logMessage, 3000);
 
-const throttledLogMessage = myThrottle(logMessage, 3000);
-
-throttledLogMessage("Hello, World!"); // Will log "Hello, World!" immediately
-throttledLogMessage("Hello again!"); // Will be ignored since it's within the throttle interval
+throttledLogMessageTrailing("Hello, World!"); // Will log "Hello, World!" immediately
+throttledLogMessageTrailing("Hello again!"); // Will be ignored but stored as lastArgs
 
 setTimeout(() => {
-  throttledLogMessage("Hello after 3 seconds!"); // Will log "Hello after 3 seconds!" after 3 seconds
-}, 3000);   
+  throttledLogMessageTrailing("Hello after 3 seconds!"); // Will log "Hello again!" immediately after 3 seconds, and "Hello after 3 seconds!" will be ignored
+}, 3000);
+
+
+function myThrottleBoth(cb, interval) {
+  let isThrottled = false;
+  let lastArgs = null;
+  let lastThis = null;
+
+  return function (...args) {
+    if (!isThrottled) {
+      // Leading call
+      cb.apply(this, args);
+      isThrottled = true;
+
+      const run = () => {
+        if (lastArgs) {
+          // Run trailing call
+          cb.apply(lastThis, lastArgs);
+          lastArgs = null;
+          lastThis = null;
+
+          // Continue cycle
+          setTimeout(run, interval);
+        } else {
+          // Stop cycle
+          isThrottled = false;
+        }
+      };
+
+      setTimeout(run, interval);
+    } else {
+      // Save latest call
+      lastArgs = args;
+      lastThis = this;
+    }
+  };
+}
